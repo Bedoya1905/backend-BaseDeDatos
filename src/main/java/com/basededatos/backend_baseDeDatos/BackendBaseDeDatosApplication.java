ec30a93595d9a3e61a63ddcd03cfb78025fb2c59
Bedoya1905 <santiagobedgo@unisabana.edu.co>
2025-10-24 08:36:32 -0500

    Agregado documentos

---
+++ src/main/java/com/basededatos/backend_baseDeDatos/BackendBaseDeDatosApplication.java
@@ -0,0 +1,13 @@
+package com.basededatos.backend_baseDeDatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBaseDeDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBaseDeDatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basededatos/backend_baseDeDatos/BackendBaseDeDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basededatos.backend_baseDeDatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBaseDeDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
